d98982c02bfa3123fa5d35acd04bfad70aea0978
Adrian Hakim Utomo <adrianhakimutomo@gmail.com>
2021-12-31 16:03:49 +0700

    views for flutter

--- infovaksin/urls.py
+++ infovaksin/urls.py
@@ -1,8 +1,10 @@
+from os import name
 from django.urls import path
 from .views import*
 
 
 urlpatterns = [
     path('', index, name='index_infovaksin'),
-    path('show_json', vaksindata, name='show_json')
+    path('show_json', vaksindata, name='show_json'),
+    path('infovaksin_comment', AddCommentFlutter, name='infovaksin_comment')
 ]
